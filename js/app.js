6d6c8c9f61b2e1cc47a651a471a2882d6d591820
christopheryoder <christophercyoder@gmail.com>
2014-08-26 17:00:39 -0400

    Added the Hadouken sound

--- js/app.js
+++ js/app.js
@@ -7,21 +7,13 @@ $(document).ready(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
-  	.mousedown(function() {
-  	// play hadouken sound
-	  	$('.ryu-ready').hide();
-	  	$('.ryu-throwing').show();
-	    $('.hadouken').show()
-	    .animate(
-	    	{'left':'300px'},
-	    	500,
-	    	function(){
-	    		$(this).hide();
-	    		$(this).css('left','-212px');
-	    	}
-    	);
-    }
-    /*
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken')
+    .finish()
+    .show()
     .animate(
   {'left': '300px'},
   500,
@@ -30,13 +22,15 @@ $(document).ready(function() {
     $(this).css('left', '-212px');
   }
 );
-  */
-    // show hadouken and animate it to the right of the screen
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
-  	$('.ryu-ready').show();
-    // ryu goes back to his ready position
+    $('.ryu-ready').show();
   });
 });
 
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+};
